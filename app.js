85077eb9ed6b81389e2d08c588f650665b733b24
Olamide <olakehinde59@gmail.com>
2019-11-06 12:40:26 +0100

    finished adding a delete method, showAlert method in the Ui class

--- app.js
+++ app.js
@@ -52,6 +52,25 @@ class UI {
 
 	}
 
+	// delete Book
+	static deleteBook(el) {
+		if (el.classList.contains('delete')) {
+			el.parentElement.parentElement.remove();
+		}
+	}
+
+	// Show Alert messages
+	static showAlert(msg, className) {
+		const div = document.createElement('div');
+
+		div.className = `alert alert-${className}`;
+		div.appendChild(document.createTextNode(msg));
+		const container = document.querySelector('.container');
+		const form  = document.querySelector('#book-form');
+
+		container.insertBefore(div, form);
+	}
+
 	// Clear all input fields after submitting
 	static clearFields () {
 		document.querySelector('#title').value = '';
@@ -72,14 +91,25 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 	const author = document.querySelector('#author').value;
 	const isbn = document.querySelector('#isbn').value;
 
-	// Instatiantiat Book Class
-	const book = new Book(title, author, isbn);
+	// validate form input
+	if (title === '' || author === '' || isbn === '') {
+		UI.showAlert('All fields must be filled', 'danger');
+	}
+	else {
+		// Instatiantiat Book Class
+		const book = new Book(title, author, isbn);
 
-	// Add new Book entry to UI
-	UI.addBooksTolist(book);
+		// Add new Book entry to UI
+		UI.addBooksTolist(book);
 
-	//	clear input fields
-	UI.clearFields(); 
+		//	clear input fields
+		UI.clearFields(); 
+	}
+
+	
 });
 
 // Events: Remove book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+	UI.deleteBook(e.target);
+});
