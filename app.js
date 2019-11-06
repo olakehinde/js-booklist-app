ee5d13dcf31d25dd6d7c28aef61f89c6ad334b15
Olamide <olakehinde59@gmail.com>
2019-11-06 12:01:11 +0100

    finished adding function to create book and display the created book in the UI

--- app.js
+++ app.js
@@ -30,15 +30,56 @@ class UI {
 
 		const books = StoredBooks;
 
-		
+		books.forEach((book) => UI.addBooksTolist(book));
+	}
+
+	static addBooksTolist(book) {
+		const list = document.querySelector('#book-list'); 
+
+		const row = document.createElement('tr');
+
+		row.innerHTML = `
+			<td>${book.title}</td>
+			<td>${book.author}</td>
+			<td>${book.isbn}</td>
+			<td>
+				<a href="#" class="btn btn-danger btn-xs delete">x</a>
+			</td>
+		`;
+
+		list.appendChild(row);
+
+
+	}
+
+	// Clear all input fields after submitting
+	static clearFields () {
+		document.querySelector('#title').value = '';
+		document.querySelector('#author').value = '';
+		document.querySelector('#isbn').value = '';
 	}
 }
 // Store class: Handles data storage (Local Storage)
 
 // Events: Display books
-
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 // Events: Add book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+	e.preventDefault();
+	// get form input values
+	const title = document.querySelector('#title').value;
+	const author = document.querySelector('#author').value;
+	const isbn = document.querySelector('#isbn').value;
 
+	// Instatiantiat Book Class
+	const book = new Book(title, author, isbn);
+
+	// Add new Book entry to UI
+	UI.addBooksTolist(book);
+
+	//	clear input fields
+	UI.clearFields(); 
+});
 
 // Events: Remove book
